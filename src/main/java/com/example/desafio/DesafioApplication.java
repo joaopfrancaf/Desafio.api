1b2b56b85c1f6808cedae1e77e4447e961db11ce
joaopfrancaf <joaopfrancaf@gmail.com>
2021-08-05 19:35:37 -0300

    simples mensagem no metodo main

--- src/main/java/com/example/desafio/DesafioApplication.java
+++ src/main/java/com/example/desafio/DesafioApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DesafioApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioApplication.class, args);		
+		SpringApplication.run(DesafioApplication.class, args);	
+		
+		System.out.println("teste git");
 	}
 }
