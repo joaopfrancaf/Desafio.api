2870f62ed80901f27b0f001813d27ae7f3ac647e
joaopfrancaf <78824866+joaopfrancaf@users.noreply.github.com>
2021-08-13 22:12:57 -0300

    Delete src/main/java/com/example/desafio directory
    socorro

--- src/main/java/com/example/desafio/repository/PautaParticipanteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.desafio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.desafio.entity.PautaParticipante;
-
-public interface PautaParticipanteRepository extends JpaRepository<PautaParticipante, Long> {
-
-}
--- src/main/java/com/example/desafio/repository/PautaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.desafio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.desafio.entity.Pauta;
-
-public interface PautaRepository extends JpaRepository<Pauta, Long> {
-
-}
